4b0179d2b633a56fd5d910a03d10106939da3d52
Michael Peterson <MadMathMike@gmail.com>
2025-08-08 17:04:05 -0600

    Successfully auth using /api/v1/auth/login endpoint of REST API

--- src/main.rs
+++ src/main.rs
@@ -1,210 +1,53 @@
-use blowfish::cipher::{BlockEncrypt, BlockSizeUser, KeyInit};
-use blowfish::{Blowfish, BlowfishLE};
-
-use cipher::BlockDecrypt;
-
 use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
 use rodio::{Decoder, OutputStreamBuilder};
-use serde::Deserialize;
 use serde_json::json;
 use std::fs::File;
-use std::io::Read;
 use std::thread::sleep;
-use std::time::SystemTime;
-
-#[derive(Debug, Deserialize)]
-struct PartnerAuthResult {
-    #[serde(rename = "partnerId")]
-    pub partner_id: String,
-    #[serde(rename = "partnerAuthToken")]
-    pub partner_auth_token: String,
-    #[serde(rename = "syncTime")]
-    pub sync_time: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct PartnerAuthResponse {
-    pub stat: String,
-    pub result: PartnerAuthResult,
-}
-
-#[derive(Debug, Deserialize)]
-struct PandoraResponse {
-    pub stat: String,
-}
-
-// pandora request failure body: {"stat":"fail","message":"An unexpected error occurred","code":6}
-
-#[derive(Debug, Deserialize)]
-struct UserAuthResult {
-    #[serde(rename = "userAuthToken")]
-    pub user_auth_token: String,
-    pub username: String,
-    #[serde(rename = "canListen")]
-    pub can_listen: bool,
-    #[serde(rename = "userId")]
-    user_id: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct UserAuthResponse {
-    pub stat: String,
-    pub result: UserAuthResult,
-}
-
-// TODO: introduce logging
 
 fn main() {
-    let partner_auth_body = json!({
-        "username": "pandora one",
-        "password": "TVCKIBGS9AO9TSYLNNFUML0743LH82D",
-        "deviceModel": "D01",
-        "version": "5"
-    });
+    let mut default_headers = HeaderMap::default();
+    default_headers.append(USER_AGENT, HeaderValue::from_static("rusty-piano/0.1"));
 
-    let request_uri = "https://internal-tuner.pandora.com/services/json/?method=auth.partnerLogin";
+    let client = Client::builder()
+        .cookie_store(true)
+        .default_headers(default_headers)
+        .build()
+        .unwrap();
 
-    // Send auth request
-    let client = Client::new();
+    let head_response = client
+        .head("https://pandora.com")
+        .header(USER_AGENT, "rusty-piano/0.1")
+        .send()
+        .expect("Error making HEAD request to root domain");
 
-    let auth_response = client
-    .post(request_uri)
-    .json(&partner_auth_body)
-    .send()
-    .expect("Error making auth call");
-    
-    // TODO: Error out on response status code != 200
+    for cookie in head_response.cookies() {
+        println!("{cookie:?}");
+    }
 
-    let partner_request_time = now_seconds();
-    println!("Partner request time: {:?}", partner_request_time);
-
-
-    let auth_response = auth_response
-        .json::<PartnerAuthResponse>()
-        .expect("Failed to parse partner auth response");
-    println!("{:?}", auth_response.result);
-
-    // TODO: calculate sync time
-    let decryption_key = "U#IO$RZPAB%VX2";
-
-    let sync_time_bytes = decode_and_decrypt(decryption_key, &auth_response.result.sync_time);
-    
-    // Skip 4 bytes of garbage per documentation: https://6xq.net/pandora-apidoc/json/authentication/#partner-login
-    // Also ignore the last two bytes. Manual testing showed they were ASCII spaces.
-    let sync_time_string = String::from_utf8(sync_time_bytes[4..14].to_vec()).unwrap();
-    let sync_time = sync_time_string.parse::<u64>().unwrap();
-    println!("Sync time: {:?}", sync_time);
-
-    let time_offset = partner_request_time - sync_time;
-
-    let partner_id = &auth_response.result.partner_id;
-    // The empty auth_token parameter is important. API returns error 3 (URL_PARAM_MISSING_AUTH_TOKEN) without it
-    let request_uri = format!(
-        "https://internal-tuner.pandora.com/services/json/?method=auth.userLogin&partner_id={partner_id}&auth_token="
-    );
-
-    // TODO: pull password from environment variable (or something)
-    let user_auth_body = json!({
-        "loginType": "user",
-        "username": "MichaelPeterson27@live.com",
+    // TODO: securely retrieve password
+    let login_body = json!(
+    {
+        "keepLoggedIn": true,
         "password": "TODO",
-        "partnerAuthToken": auth_response.result.partner_auth_token.clone(),
-        "syncTime": now_seconds() + time_offset
+        "username": "MichaelPeterson27@live.com"
     });
 
-    let user_auth_body_as_str = user_auth_body.to_string();
-
-    let encryption_key = "2%3WCL*JU$MP]4";
-    let encrypted_body = encrypt(encryption_key, &user_auth_body_as_str);
-
-    let auth_response = client
-        .post(request_uri)
-        .body(encrypted_body)
+    let login_response = client
+        .post("https://pandora.com/api/v1/auth/login")
+        .header(USER_AGENT, "rusty-piano/0.1")
+        .json(&login_body)
         .send()
-        .expect("Error making user auth call");
+        .expect("Error building loging request");
 
-    // TODO: eventually logging should maybe write this to console or to a file when the right level is set
-    // println!("{}", auth_response.text().unwrap());
+    // TODO: parse return value
 
-    // TODO: Error out on response status code != 200
+    // TODO: store auth token (for subsequent calls and for next session to bypass login)
 
-    let user_auth_response = auth_response
-        .json::<UserAuthResponse>()
-        .expect("Failed to parse user auth response");
-    println!("{:?}", user_auth_response);
-
-    // TODO: call user.getStationList
-    let auth_token = user_auth_response.result.user_auth_token;
-    let user_id = user_auth_response.result.user_id;
-    let request_uri = format!(
-        "https://internal-tuner.pandora.com/services/json/?method=user.getStationList&partner_id={partner_id}&user_id={user_id}&auth_token="
-    );
-
-    let get_station_list_body = json!({
-        "userAuthToken": auth_token,
-        "syncTime": now_seconds() + time_offset,
-        "returnAllStations": true
-    });
-
-    let get_station_list_body_as_str = get_station_list_body.to_string();
-    let encrypted_body = encrypt(encryption_key, &get_station_list_body_as_str);
-
-    let get_station_list_response = client
-        .post(request_uri)
-        .body(encrypted_body)
-        .send()
-        .expect("Error making get station list call");
-
-    println!("{}", get_station_list_response.text().unwrap());
+    println!("{:?}", login_response.status());
+    println!("{}", login_response.text().unwrap());
 
     play_sample_sound();
-}
-
-fn now_seconds() -> u64 {
-    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
-}
-
-fn encrypt(key: &str, input: &str) -> String {
-    let blowfish = Blowfish::<byteorder::BigEndian>::new_from_slice(key.as_bytes()).unwrap();
-    let block_size = BlowfishLE::block_size(); // should be 8
-
-    let plaintext = input.as_bytes();
-    let padded_len = plaintext.len().div_ceil(block_size) * block_size;
-    let mut padded_plaintext = vec![0u8; padded_len];
-    padded_plaintext[..plaintext.len()].copy_from_slice(plaintext);
-
-    let mut ciphertext = vec![0u8; padded_len];
-
-    for (in_block, out_block) in padded_plaintext
-        .chunks(block_size)
-        .zip(ciphertext.chunks_mut(block_size))
-    {
-        blowfish.encrypt_block_b2b(in_block.into(), out_block.into());
-    }
-
-    hex::encode(&ciphertext)
-}
-
-fn decode_and_decrypt(key: &str, input: &str) -> Vec<u8> {
-    let decoded_input: Vec<u8> = hex::decode(input).expect("Error hex decoding input string");
-
-    decrypt(key, decoded_input)
-}
-
-fn decrypt(key: &str, input: Vec<u8>) -> Vec<u8> {
-    let blowfish = Blowfish::<byteorder::BigEndian>::new_from_slice(key.as_bytes()).unwrap();
-    let block_size = BlowfishLE::block_size(); // should be 8
-
-    let mut plaintext = vec![0u8; input.len()];
-
-    for (in_block, out_block) in input
-        .chunks(block_size)
-        .zip(plaintext.chunks_mut(block_size))
-    {
-        blowfish.decrypt_block_b2b(in_block.into(), out_block.into());
-    }
-
-    plaintext
 }
 
 fn play_sample_sound() {
@@ -220,22 +63,4 @@ fn play_sample_sound() {
     stream_handle.mixer().add(source);
 
     sleep(std::time::Duration::from_secs(5));
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn encrypt_test_vector() {
-        let encrypted = encrypt("R=U!LH$O2B#", "è.<Ú1477631903");
-        assert_eq!(encrypted, "4a6b45612b018614c92c50dc73462bbd");
-    }
-
-    #[test]
-    fn decrypt_test_value() {
-        let decrypted = decode_and_decrypt("R=U!LH$O2B#", "4a6b45612b018614c92c50dc73462bbd");
-        let decrypted = String::from_utf8(decrypted).unwrap();
-        assert_eq!(decrypted, "è.<Ú1477631903");
-    }
 }
