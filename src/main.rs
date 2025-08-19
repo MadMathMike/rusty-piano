bb9dec8b5f1d1e6b124eac63adf60845a3cc741a
Michael Peterson <MadMathMike@gmail.com>
2025-08-18 20:24:03 -0600

    Exit with ctrl+c

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use color_eyre::Result;
 use crossterm::event::{KeyCode, KeyEventKind};
-use ratatui::{layout::{Constraint, Direction, Layout}, widgets::{Block, Borders, Paragraph}};
+use ratatui::{
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Paragraph},
+};
 
-type Result = color_eyre::Result<()>;
-
-fn main() -> Result {
+fn main() -> Result<()> {
+    color_eyre::install()?;
     let mut terminal = ratatui::init(); // Puts the terminal in raw mode, which disables line buffering (so rip to ctrl+c response)
 
     let mut app = App::default();
@@ -11,20 +14,21 @@ fn main() -> Result {
     app.run(&mut terminal)?;
 
     ratatui::restore(); // Returns the terminal back to normal mode
-    
-    Ok(()) 
+
+    Ok(())
 }
 
 #[derive(Debug, Default)]
 pub struct App {
-    exit: bool
+    exit: bool,
 }
 
 impl App {
-    fn run(&mut self, terminal: &mut ratatui::Terminal<ratatui::prelude::CrosstermBackend<std::io::Stdout>>) -> Result {
-        
+    fn run(
+        &mut self,
+        terminal: &mut ratatui::Terminal<ratatui::prelude::CrosstermBackend<std::io::Stdout>>,
+    ) -> Result<()> {
         while !self.exit {
-
             terminal.draw(|frame| {
                 let outer_layout = Layout::default()
                     .direction(Direction::Vertical)
@@ -38,26 +42,28 @@ impl App {
 
                 let body = Layout::default()
                     .direction(Direction::Horizontal)
-                    .constraints(vec![
-                        Constraint::Percentage(40),
-                        Constraint::Percentage(60),
-                    ])
+                    .constraints(vec![Constraint::Percentage(40), Constraint::Percentage(60)])
                     .split(outer_layout[1]);
-                    
+
                 frame.render_widget("hello world", outer_layout[0]);
                 frame.render_widget(
                     Paragraph::new("Left").block(Block::new().borders(Borders::ALL)),
-                    body[0]
+                    body[0],
                 );
                 frame.render_widget(
                     Paragraph::new("Right").block(Block::new().borders(Borders::ALL)),
-                    body[1]
+                    body[1],
                 );
                 frame.render_widget("woah", outer_layout[2]);
             })?;
 
             if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
-                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
+                if key_event.kind == KeyEventKind::Press
+                    && key_event.code == KeyCode::Char('c')
+                    && key_event
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL)
+                {
                     self.exit = true;
                 }
             }
